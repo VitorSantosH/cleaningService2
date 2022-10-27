121a1200690768caf73cfe4fae2b025750bfaf78
VitorSantosH <wytorh@gmail.com>
2022-10-26 23:29:35 -0300

    primeiro commit

---
+++ build/precache-manifest.66b6a8453f5df0ecca696c9b014d7640.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = [
+  {
+    "revision": "330579d779b398e34133",
+    "url": "/static/css/main.42b7338e.chunk.css"
+  },
+  {
+    "revision": "330579d779b398e34133",
+    "url": "/static/js/main.330579d7.chunk.js"
+  },
+  {
+    "revision": "ac5b1bae6ab4fa649ed7",
+    "url": "/static/js/1.ac5b1bae.chunk.js"
+  },
+  {
+    "revision": "72af9770d1909a3ffbec",
+    "url": "/static/js/2.72af9770.chunk.js"
+  },
+  {
+    "revision": "8e3a2bdfbbedc326a1c5",
+    "url": "/static/js/runtime~main.8e3a2bdf.js"
+  },
+  {
+    "revision": "f7eaa686d6fd3a1f961aff84e8691ee3",
+    "url": "/static/media/índice13.f7eaa686.jpg"
+  },
+  {
+    "revision": "faefb62c404843868e98a6b09bff75d5",
+    "url": "/static/media/logoMelhor.faefb62c.png"
+  },
+  {
+    "revision": "2d2f618453e464995cf950d6646e54c4",
+    "url": "/static/media/hero-bg-6.2d2f6184.jpg"
+  },
+  {
+    "revision": "a143b934f45fc3aad5f0730a3f5fa8e7",
+    "url": "/static/media/slider4.a143b934.jpg"
+  },
+  {
+    "revision": "cfe109250a1e601e676266d42e85301f",
+    "url": "/static/media/hero-bg-5.cfe10925.jpg"
+  },
+  {
+    "revision": "d820aece75491d894e987cb9f1084bc7",
+    "url": "/static/media/room-cleaning.d820aece.jpg"
+  },
+  {
+    "revision": "6abcbf0e9d2314ed0473f444f528aaa6",
+    "url": "/static/media/window-clining.6abcbf0e.jpg"
+  },
+  {
+    "revision": "777d34a3636cb29147c863bbda51973c",
+    "url": "/static/media/glass-cleaning.777d34a3.jpg"
+  },
+  {
+    "revision": "6a02f3a7f07643f034e412813bbc2ad6",
+    "url": "/static/media/índice1.6a02f3a7.jpg"
+  },
+  {
+    "revision": "21a0438d1dedf1b5746e63d0fc17aa07",
+    "url": "/static/media/índice3.21a0438d.jpg"
+  },
+  {
+    "revision": "8bbbee77c23989dfca399b815ba79b11",
+    "url": "/static/media/índice4.8bbbee77.jpg"
+  },
+  {
+    "revision": "2f42647ce79a00e0d731e03480502aad",
+    "url": "/static/media/índice5.2f42647c.jpg"
+  },
+  {
+    "revision": "06b28a7d075214cd80214ce379d48893",
+    "url": "/static/media/índice6.06b28a7d.jpg"
+  },
+  {
+    "revision": "ebbdc0f35be6252df272c389d5b0b179",
+    "url": "/static/media/índice7.ebbdc0f3.jpg"
+  },
+  {
+    "revision": "81407f92800278fe456f5d75a26000d4",
+    "url": "/static/media/índice8.81407f92.jpg"
+  },
+  {
+    "revision": "50e7f2fb9bc0b779e6fa0b3da0faf1b5",
+    "url": "/static/media/índice15.50e7f2fb.jpg"
+  },
+  {
+    "revision": "a8ca7ee07e9c1d36310352fc40271910",
+    "url": "/static/media/índice16.a8ca7ee0.jpg"
+  },
+  {
+    "revision": "e841f65f0eb2cd6a1b6ba4a8db35ad52",
+    "url": "/static/media/índice17.e841f65f.jpg"
+  },
+  {
+    "revision": "cc717eb751d4966ff2d6cf6b95cf50ff",
+    "url": "/static/media/índice18.cc717eb7.jpg"
+  },
+  {
+    "revision": "31a0835c64278d103a7dd01f8812f56f",
+    "url": "/static/media/pngwing.31a0835c.png"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "7cf3a627711c26616363209aa245bee9",
+    "url": "/static/media/kitchen-cleaning.7cf3a627.jpg"
+  },
+  {
+    "revision": "47da52bac3fc7b4cf72dcf810efb5a6b",
+    "url": "/static/media/toilet-cleaning.47da52ba.jpg"
+  },
+  {
+    "revision": "33757e8d83d61765634686c471249f32",
+    "url": "/static/media/about-img.33757e8d.png"
+  },
+  {
+    "revision": "523758373232e366c85dc143c15c9fcb",
+    "url": "/index.html"
+  }
+];
