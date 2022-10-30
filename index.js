1fcc23c17d28f01ff509147c1f1fd8f6f5f3e464
VitorSantosH <wytorh@gmail.com>
2022-10-30 14:18:58 -0300

    correcao 2.8

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+
+// instancia express
+const app = express();
+const port = 443;
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+app.use('/', express.static("build") );
+
+
+
+
+app.listen(port, () => {
+    console.log("servidor funcionando na porta: " + port)
+})
