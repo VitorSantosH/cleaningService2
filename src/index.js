85358ed98f143e6e823913692078a9dcd60e2fc6
VitorSantosH <wytorh@gmail.com>
2023-07-12 23:17:15 -0300

    http e https

--- src/index.js
+++ src/index.js
@@ -1,23 +1,39 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/cleaningservicesperfect.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync("/etc/letsencrypt/live/cleaningservicesperfect.com/fullchain.pem", 'utf8');
+var credentials = { key: privateKey, cert: certificate };
 
 
 // instancia express
 const app = express();
-const port = 80;
+const portHttp = 80;
+const portHttpS = 443
 
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('combined'));
+app.use('/', express.static("build"));
 
-app.use('/', express.static("build") );
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(portHttp, function () {
+    console.log("JSON Server is running on " + portHttp);
+});
+httpsServer.listen(portHttpS, function () {
+    console.log("JSON Server is running on :" + portHttpS);
+})
 
 
 
-
-app.listen(port, () => {
+/*app.listen(port, () => {
     console.log("servidor funcionando na porta: " + port)
 })
+*/
